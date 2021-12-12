24ebb7b98ac4ff0d2089d7aad9bef076dc883721
91639 <goel.aryan2406@gmail.com>
2021-12-12 15:35:10 +0530

    home page and header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <div>
+           Header 
+        </div>
+    )
+}
+import styled from 'styled-components';
+
+export default Header
