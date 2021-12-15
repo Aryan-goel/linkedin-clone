c8b0c38b3d7274d4b4d3aa71f4b02095c50c426f
91639 <goel.aryan2406@gmail.com>
2021-12-15 18:49:36 +0530

    search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,9 @@ function Header() {
         </Logo>
         <Search>
           <div>
-            <input type="text" placehoder="Search" />
+            <input type="text" placeholder="Search" />
           </div>
-          <SeachIcon>
+          <SearchIcon>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -22,11 +22,10 @@ function Header() {
               fill="currentColor"
               width="16"
               height="16"
-              focusable="false"
-            >
+              focusable="false">
               <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
             </svg>
-          </SeachIcon>
+          </SearchIcon>
         </Search>
       </Content>
     </Container>
@@ -56,11 +55,34 @@ const Logo = styled.span`
   font-size: 0px;
 `;
 const Search = styled.div`
-opacity:1;
-flex-grow:1;
-position:relative;
-& > div{
-    max-width:28
-}
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 28px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 218px;
+      padding: 0 0px 0 40px;
+      line-height: 1.75;
+      font-weight:400;
+      font-size:14px;
+      height:34px;
+      border-color:#dce6f1;
+      vertical-align:text-top;
+    }
+  }
 `;
-const SeachIcon = styled.div``;
+const SearchIcon = styled.div`
+
+width:40px;
+position:absolute;
+z-index:1;
+top:10px;
+left:2px;
+
+`;
