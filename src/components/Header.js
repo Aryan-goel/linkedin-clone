e7898d3fa88270008b43b5090d362febe4c60e79
91639 <goel.aryan2406@gmail.com>
2021-12-16 02:57:09 +0530

    nav bar init

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,11 +15,17 @@ function Header() {
             <input type="text" placeholder="Search" />
           </div>
           <SearchIcon>
-           <img src="/images/search-icon.svg" alt=""></img> 
+            <img src="/images/search-icon.svg" alt=""></img>
           </SearchIcon>
         </Search>
         <Nav>
-          Nav menus
+          <NavListWrap>
+            <NavList>
+              <a>
+                <img src="/images/nav-home.svg" alt="" />
+              </a>
+            </NavList>
+          </NavListWrap>
         </Nav>
       </Content>
     </Container>
@@ -63,40 +69,66 @@ const Search = styled.div`
       width: 218px;
       padding: 0 0px 0 40px;
       line-height: 1.75;
-      font-weight:400;
-      font-size:14px;
-      height:34px;
-      border-color:#dce6f1;
-      vertical-align:text-top;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
     }
   }
 `;
 const SearchIcon = styled.div`
-
-width:40px;
-position:absolute;
-z-index:1;
-top:10px;
-left:2px;
-border-radius:0 2px 2px 0;
-margin:0;
-pointer-events:none; 
+  width: 40px;
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  left: 2px;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.15s;
+`;
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+const NavListWrap = styled.ul`
 display:flex;
-justify-content:center;
-align-items:center;
-transition:background-color 0.15s
+flex-wrap:nowrap;
+list-style-type:none;
 
 `;
-const Nav= styled.nav`
-margin-left:auto;
-display:block;
-@media(max-width:768px){
-  position:fixed;
-  left:0;
-  bottom:0;
-  background:white;
-  width:100%;
+const NavList = styled.li`
+display:flex
+align-items:center;
+a{
+  align-items:center;
+  background:transparent;
+  display:flex;
+  flex-direction:column;
+  font-size:12px;
+  font-weight:400;
+  justify-content:center;
+  line-height:1.5;
+  min-height:42px;
+  mid-width:80px;
+  position:relative;
+  text-decoration:none;
+span{
   
+}
+
 
 }
-`
+
+`;
