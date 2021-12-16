d03a4949cdf29baf0a9948e63e8b6c938d162292
91639 <goel.aryan2406@gmail.com>
2021-12-16 23:53:05 +0530

    main content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
+function Home() {
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? -   </a>
+        </h5>
+        <p>   Find talented in record time with Upwork and keep business moving</p>
+      </Section>
+    </Container>
+  );
+}
+const Container=styled.div`
+padding-top:40px;
+max-width:100%;
 
-function Home() { 
-    return (
-        <div>
-            Home
-        </div>
-    )
+`
+const Content=styled.div`
+max-width:1128px;
+margin-left:auto;
+
+margin-right:auto;
+`
+const Section=styled.section`
+min-height:50px;
+padding:16px 0;
+box-sizing:content-box;
+text-align:center;
+text-decoration:underline;
+display:flex;
+justify-content:center;
+align-items:center;
+h5 {
+   color:#0a66c2;
+   font-size:14px;
+   a {
+    font-weight:700;   
+   }
+}
+p{
+    font-size:14px;
+    color:#434649;
+    font-weight:600;
 }
 
-export default Home
+
+`
+export default Home;
