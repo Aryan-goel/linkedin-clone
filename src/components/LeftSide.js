3d2e2a67d7edc92a192f2973c6d1cac81b5ca5e2
91639 <goel.aryan2406@gmail.com>
2021-12-19 15:53:55 +0530

    left side structuring

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -2,14 +2,39 @@ import React from "react";
 import styled from "styled-components";
 
 function LeftSide() {
-  return <Container>left side</Container>;
+  return <Container>
+    <Artcard>
+      <UserInfo>
+        <Cardbackground/>
+        <a>
+          <Photo/>
+          <Link> Welcome, there!</Link>
+        </a>
+        <a>
+          <AddPhoto>
+            add a photo
+          </AddPhoto>
+        </a>
+      </UserInfo>
+    </Artcard>
+  </Container>;
 }
 
 export default LeftSide;
 
+const Container = styled.div`
+  grid-area: LeftSide;
+`;
+const Artcard=styled.div`
 
-const Container=styled.div`
-grid-area:LeftSide;
-
+text-align:center;
+overflow:hidden;
+margin-bottom:8px;
+background-color:#fff;
+border-radius:5px;
+transition:box-shadow:83ms;
+position:relative;
+border:none;
+box-shadow: 0 0 0 1px rgb( 0 0 0 /15%),0 0 0 rgb(0 0 0/20%);
 
 `
