e03ee98ccb5ebfc8950836ee852c2f66b12f98ef
91639 <goel.aryan2406@gmail.com>
2021-12-19 22:48:26 +0530

    user info updated

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -9,12 +9,27 @@ function LeftSide() {
           <Cardbackground />
           <a>
             <Photo />
-            <Link> Welcome, there!</Link>
+            <Link> Welcome, There!</Link>
           </a>
           <a>
-            <AddPhoto>add a photo</AddPhoto>
+            <AddPhoto>Add a photo</AddPhoto>
           </a>
         </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your network</span>
+            </div>
+            <img src="/images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" alt="" />
+            My Items
+          </span>
+        </Item>
       </Artcard>
     </Container>
   );
@@ -23,7 +38,7 @@ function LeftSide() {
 export default LeftSide;
 
 const Container = styled.div`
-  grid-area:leftside;
+  grid-area: leftside;
 `;
 const Artcard = styled.div`
 
@@ -38,44 +53,99 @@ border:none;
 box-shadow: 0 0 0 1px rgb( 0 0 0 /15%),0 0 0 rgb(0 0 0/20%);
 
 `;
-const UserInfo=styled.div`
+const UserInfo = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding: 12px 12px 16px;
+  word-wrap: break-word;
+  word-break: break-word;
+`;
+const Cardbackground = styled.div`
+  background: url("/images/card-bg.svg");
+  background-position: center;
+  background-size: 462px;
+  height: 54px;
+  margin: -12px -12px 0;
+`;
+const Photo = styled.div`
+  box-shadow: none;
+  background-image: url("/images/photo.svg");
+  width: 72px;
+  height: 72px;
+  box-sizing: border-box;
+  background-clip: content-box;
+  background-position: center;
+  background-size: 60%;
+  background-repeat: no-repeat;
+  border: 2px solid white;
+  margin: -38px auto 12px;
+  border-radius: 50%;
+`;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
+const AddPhoto = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 400;
+`;
+const Widget = styled.div`
 border-bottom:1px solid rgba(0,0,0,0.15);
-padding:12px 12px 16px;
-word-wrap:break-word;
-word-break:break-word;
-`
-const Cardbackground=styled.div`
-background:url("/images/card-bg.svg");
-background-position:center;
-background-size:462px;
-height:54px;
-margin:-12px -12px 0;
+padding-top:12px;
+padding-bottom:12px;
+ &> a{
+   text-decoration:none;
+   display:flex;
+   justify-content:space-between;
+   align-items:center;
+  padding:4px 12px;
+  &:hover{
+    background-color:rgba(0,0,0,0.08);
+  }
+  div{
+    display:flex;
+    flex-direction:column;
+    text-align:left;
+    span{
+      font-size:12px;
+      line-height:1.333;
+      &:first-child{
+        color:rgba(0,0,0,0.6);
+      }
+      &:nth-child(2){
+        color:rgba(0,0,0,1);
 
-`
-const Photo=styled.div`
- box-shadow:none;
- background-image:url("/images/photo.svg");
-width:72px;
-height:72px;
-box-sizing:border-box;
-background-clip:content-box;
-background-position:center;
-background-size:60%;
-background-repeat:no-repeat;
-border:2px solid white;
-margin:-38px auto 12px;
-border-radius:50%
-`
-const Link= styled.div`
-font-size:16px;
-line-height:1.5;
-color:rgba(0, 0, 0, 0.9);
-font-weight:600;
-`
-const AddPhoto=styled.div`
-color:#0a66c2;
-margin-top:4px;
+      }
+    }
+
+  }
+ }
+ svg{
+   color:rgba(0,0,0,1);
+ }
+
+`;
+const Item = styled.a`
+border-color:rgba(0,0,0,0.8);
+text-align:left;
+padding:12px;
 font-size:12px;
-line-height:1.33;
-font-weight:400;
-`
+display:block;
+span{
+  display:flex;
+  align-items:center;
+  color:rgba(0,0,0,1);
+  svg{
+    color:rgba(0,0,0,0.6);
+  }
+}
+&:hover{
+  background-color:rgba(0,0,0,0.08);
+
+}
+
+`;
