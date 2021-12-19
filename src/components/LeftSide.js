5ef1937f04e538970863476e701875451cfc1e76
91639 <goel.aryan2406@gmail.com>
2021-12-19 17:28:05 +0530

    user info

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -42,13 +42,40 @@ const UserInfo=styled.div`
 border-bottom:1px solid rgba(0,0,0,0.15);
 padding:12px 12px 16px;
 word-wrap:break-word;
+word-break:break-word;
 `
 const Cardbackground=styled.div`
+background:url("/images/card-bg.svg");
+background-position:center;
+background-size:462px;
+height:54px;
+margin:-12px -12px 0;
 
 `
 const Photo=styled.div`
+ box-shadow:none;
+ background-image:url("/images/photo.svg");
+width:72px;
+height:72px;
+box-sizing:border-box;
+background-clip:content-box;
+background-position:center;
+background-size:60%;
+background-repeat:no-repeat;
+border:2px solid white;
+margin:-38px auto 12px;
+border-radius:50%
 `
 const Link= styled.div`
+font-size:16px;
+line-height:1.5;
+color:rgba(0, 0, 0, 0.9);
+font-weight:600;
 `
 const AddPhoto=styled.div`
+color:#0a66c2;
+margin-top:4px;
+font-size:12px;
+line-height:1.33;
+font-weight:400;
 `
