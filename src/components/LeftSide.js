6a9399168832775b3533f0acc176e766eecb41a6
91639 <goel.aryan2406@gmail.com>
2021-12-20 22:27:44 +0530

    left side components

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -31,6 +31,23 @@ function LeftSide() {
           </span>
         </Item>
       </Artcard>
+      <CommunityCard>
+        <a>
+          <span>Groups</span>
+        </a>
+        <a>
+          <span>
+            Events
+            <img src="/images/plus-icon.svg" alt="" />
+          </span>
+        </a>
+        <a>
+          <span>Follow Hashtags</span>
+        </a>
+        <a>
+          <span>Discover More</span>
+        </a>
+      </CommunityCard>
     </Container>
   );
 }
@@ -94,58 +111,81 @@ const AddPhoto = styled.div`
   font-weight: 400;
 `;
 const Widget = styled.div`
-border-bottom:1px solid rgba(0,0,0,0.15);
-padding-top:12px;
-padding-bottom:12px;
- &> a{
-   text-decoration:none;
-   display:flex;
-   justify-content:space-between;
-   align-items:center;
-  padding:4px 12px;
-  &:hover{
-    background-color:rgba(0,0,0,0.08);
-  }
-  div{
-    display:flex;
-    flex-direction:column;
-    text-align:left;
-    span{
-      font-size:12px;
-      line-height:1.333;
-      &:first-child{
-        color:rgba(0,0,0,0.6);
-      }
-      &:nth-child(2){
-        color:rgba(0,0,0,1);
-
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.08);
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      span {
+        font-size: 12px;
+        line-height: 1.333;
+        &:first-child {
+          color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+          color: rgba(0, 0, 0, 1);
+        }
       }
     }
-
   }
- }
- svg{
-   color:rgba(0,0,0,1);
- }
-
+  svg {
+    color: rgba(0, 0, 0, 1);
+  }
 `;
 const Item = styled.a`
-border-color:rgba(0,0,0,0.8);
+  border-color: rgba(0, 0, 0, 0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+    svg {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
+const CommunityCard=styled(Artcard)`
+padding:8px 0 0 ;
 text-align:left;
-padding:12px;
-font-size:12px;
-display:block;
-span{
-  display:flex;
-  align-items:center;
-  color:rgba(0,0,0,1);
-  svg{
+display:flex;
+flex-direction:column;
+a{
+  color:black;
+  padding:4px 12px 4px 12px;
+  font-size:12px;
+  &:hover{
+    color:#0a66c2;
+  }
+  span{
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+  }
+  &:last-child{
     color:rgba(0,0,0,0.6);
+    text-decoration:none;
+    border-top:1px solid #d6cec2;
+    padding:12px;
+    &:hover{
+      background-color:rgba(0,0,0,0.08);
+    }
   }
 }
-&:hover{
-  background-color:rgba(0,0,0,0.08);
-
-}
-
-`;
+`
