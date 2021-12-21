24ec1d7ff6f0e82534511fea998cabed25b10fdf
91639 <goel.aryan2406@gmail.com>
2021-12-21 19:22:20 +0530

    following component

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -19,20 +19,20 @@ function RightSide() {
             </div>
           </li>
           <li>
-              <a>
-                  <Avatar/>
-              </a>
-              <div>
-                  <span>
-                      #Video
-                  </span>
-                  <button>
-                      Follow
-                  </button>
-                  </div>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
           </li>
-
         </FeedList>
+        <Recommendation>
+          View all Recommendations.
+          <img src="/images/right-icon.svg" alt=""/>
+        </Recommendation>
+        
       </FollowCard>
     </Container>
   );
@@ -61,7 +61,7 @@ const Title = styled.div`
   width: 100%;
   color: rgba(0, 0, 0, 0.6);
 `;
-const FeedList=styled.ul`
+const FeedList = styled.ul`
 margin-top:16px;
 li{
     display:flex;
@@ -75,8 +75,29 @@ li{
     }
     button{
         background-color:transparent;
-        color:rgba()
-    }
+        color:rgba(0,0,0,0.6);
+        box-shadow:inset 0 0 0 1px rgba(0,0,0,0.6);
+        padding:16px;
+        align-items:center;
+        border-radius:15px;
+        box-sizing:border-box;
+        font-weight:600;
+        display:inline-flex;
+        justify-content:center;
+        max-height:32px;
+        max-width:480px;
+        text-align:center;
+        outline:none;
+
 }
-`
-const Avatar=styled.div``
+`;
+const Avatar = styled.div`
+  background-image: url("https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 48px;
+  height: 48px;
+  margin-right: 8px;
+`;
+const Recommendation=styled.a``
