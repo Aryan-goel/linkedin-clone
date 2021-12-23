13351bc66ee180ac2f6f8b37c7626cad75946779
91639 <goel.aryan2406@gmail.com>
2021-12-23 11:43:31 +0530

    icons updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Main() {
-    return (
-        <Container>
-            <ShareBox>
-                share
-            </ShareBox>
-            <div>
-                <img src='/images/' alt=''/>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <ShareBox>share</ShareBox>
+      <div>
+        <img src="/images/user.svg" alt="" />
+        <button>Start a post</button>
+      </div>
+      <div>
+        <button>
+          <img src="/images/photo-icon.png " alt="" width="20px"/>
+          <span>Photo</span>
+        </button>
+        <button>
+          <img src="/images/video.png" alt=""  width="20px"/>
+          <span> Video</span>
+        </button>
+        <button>
+          <img src="/images/events.svg" alt="" width="20px" />
+          <span> Event</span>
+        </button>
+        <button>
+          <img src="/images/article.png" alt="" width="20px"/>
+          <span>Write an article</span>
+        </button>
+      </div>
+    </Container>
+  );
 }
 
 export default Main;
 
-const Container=styled.div`
-grid-area:main;
-
-`
-const CommonCard=styled.div`
-text-align:center;
-overflow:hidden;
-margin-bottom:8px;
-background-color:#fff;
-border-radius:5px;
-position:relative;
-border:none;
-box-shadow: 0 0 0 1px rgb(0 0 0/15%),0 0 0rgb(0 0 0/20%);
-
-`
-const ShareBox=styled(CommonCard)`
-display:flex;
-flex-direction:column;
-color:#958b7b;
-margin:0 0 8px;]
-background:white;
-
-`
+const Container = styled.div`
+  grid-area: main;
+`;
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0/15%), 0 0 0rgb (0 0 0/20%);
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  ]background: white;
+`;
