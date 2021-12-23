0bfa40864c7d2f67ab50d37a0527be37aee33b65
91639 <goel.aryan2406@gmail.com>
2021-12-23 10:45:23 +0530

    more icons imported

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,12 @@ import styled from 'styled-components'
 function Main() {
     return (
         <Container>
-            Main
+            <ShareBox>
+                share
+            </ShareBox>
+            <div>
+                <img src='/images/' alt=''/>
+            </div>
         </Container>
     )
 }
@@ -13,5 +18,24 @@ export default Main;
 
 const Container=styled.div`
 grid-area:main;
+
+`
+const CommonCard=styled.div`
+text-align:center;
+overflow:hidden;
+margin-bottom:8px;
+background-color:#fff;
+border-radius:5px;
+position:relative;
+border:none;
+box-shadow: 0 0 0 1px rgb(0 0 0/15%),0 0 0rgb(0 0 0/20%);
+
+`
+const ShareBox=styled(CommonCard)`
+display:flex;
+flex-direction:column;
+color:#958b7b;
+margin:0 0 8px;]
+background:white;
 
 `
