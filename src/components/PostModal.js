2ef386e11ac1a1ac6878127e3f6a8347e71cd08f
91639 <goel.aryan2406@gmail.com>
2021-12-29 12:19:41 +0530

    post modal design

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -3,21 +3,95 @@ import styled from "styled-components";
 import React from "react";
 
 function PostModal(props) {
-  return <Container>
-      postmodal
-  </Container>;
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <h2>Create a post</h2>
+          <button>
+            <img src="/images/close-icon.svg" alt="" />{" "}
+          </button>
+        </Header>
+        <Sharedcontent>
+          <Userinfo>
+            <img src="/images/user.svg" alt="" />
+            <span>Name</span>
+          </Userinfo>
+        </Sharedcontent>
+      </Content>
+    </Container>
+  );
 }
 
 export default PostModal;
 
 const Container = styled.div`
-position:fixed;
-top:0;
-left:0;
-right:0;
-bottom:0;
-z-index:9999;
-color:black;
-background-color:rgba(0,0,0,0.8);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 9999;
+  color: black;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+const Content = styled.div`
+  width: 100%;
+  max-width: 552px;
+  background-color: white;
+  max-height: 90%;
+  overflow: initial;
+  border-radius: 5px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  top: 32px;
+  margin: 0 auto;
+`;
 
+const Header = styled.div`
+  display: block;
+  padding: 16px 20px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    height: 40px;
+    width: 40px;
+    min-width: auto;
+    color: rgba(0, 0, 0, 0.15);
+    svg {
+      pointer-events: none;
+    }
+  }
+`;
+
+const Sharedcontent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+const Userinfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 24px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+  span {
+  }
 `;
